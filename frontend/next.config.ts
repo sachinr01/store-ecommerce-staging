590e2f1ed2f48025b8843f1d3679853f93d4ca01
bawanesumit01 <bawanesumit01@gmail.com>
2026-06-04 10:46:47 +0530

    again bug

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -9,8 +9,8 @@ const nextConfig: NextConfig = {
   async rewrites() {
     
     const apiBase = (process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:3000/api')
-                    .replace(/\/+$/, '')
-                    .replace(/\/\/api$/, '');
+                .replace(/\/+$/, '')
+                .replace(/\/api$/, '');
 
     return [
       {
